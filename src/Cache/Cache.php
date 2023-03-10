0d5011eb20ca6f5339e3485d6c2de06d2aea56cc
huangyaobin <huangyaobin@nicefilm.com>
2023-03-11 01:39:58 +0800

    update cache

--- src/Cache/Cache.php
+++ src/Cache/Cache.php
@@ -2,9 +2,41 @@
 
 namespace Gino\Yaf\Kernel\Cache;
 
+use Closure;
+use DateInterval;
 use Psr\Cache\CacheItemPoolInterface;
+use Psr\SimpleCache\InvalidArgumentException;
 use Symfony\Component\Cache\Psr16Cache;
 
 class Cache extends Psr16Cache {
+
+    /**
+     * @param string $key
+     * @param null|int|DateInterval $ttl
+     * @param Closure $callback
+     * @return mixed
+     * @throws InvalidArgumentException
+     */
+    public function remember(string $key, $ttl, Closure $callback) {
+        $value = $this->get($key);
+
+        if (!is_null($value)) {
+            return $value;
+        }
+
+        $this->set($key, $value = $callback(), $ttl);
+
+        return $value;
+    }
+
+    /**
+     * @param string $key
+     * @param Closure $callback
+     * @return mixed
+     * @throws InvalidArgumentException
+     */
+    public function rememberForever(string $key, Closure $callback) {
+        return $this->remember($key, null, $callback);
+    }
 
 }
